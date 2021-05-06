6ea87fdf869c6db68ea4a04afe0c5cce877e5280
Abdelrahman <said.abdelrahman89@gmail.com>
2021-05-07 00:11:01 +0100

    Completed the javascript custom form validation

---
+++ public/js/page.js
@@ -0,0 +1,32 @@
+const urlInput = document.querySelector("#long-url");
+const submitBtn = document.querySelector("#submit");
+const errorMsg = document.querySelector("#error");
+
+let errorTimeout;
+
+submitBtn.addEventListener("click", (event) => {
+    if (!urlInput.validity.valid) {
+        event.preventDefault();
+        formValidation();
+        errorTimeout = setTimeout(clearError, 3000);
+    }
+});
+
+function formValidation() {
+    clearTimeout(errorTimeout);
+
+    errorMsg.classList.add("visible");
+    errorMsg.classList.remove("hidden");
+
+    if (urlInput.validity.valueMissing) {
+        errorMsg.textContent = "Please enter a URL";
+    } else if (urlInput.validity.typeMismatch) {
+        errorMsg.textContent = "Invalid URL";
+    }
+}
+
+function clearError() {
+    errorMsg.classList.remove("visible");
+    errorMsg.classList.add("hidden");
+    errorMsg.textContent = "";
+}
