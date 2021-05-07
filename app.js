791f497d8658d0de6dcfa8e1eefd52e166d882ae
Abdelrahman <said.abdelrahman89@gmail.com>
2021-05-07 14:35:13 +0100

    Testing getting rid of the routers to see if it makes the project pass on freeCodeCamp

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const indexRouter = require("./routes/index");
-const apiRouter = require("./routes/api");
+// const indexRouter = require("./routes/index");
+// const apiRouter = require("./routes/api");
+
+const port = process.env.PORT || 3000;
 
 mongoose
     .connect(process.env.MONGO_URL, {
@@ -23,10 +25,63 @@ mongoose
 
         app.use(express.static(path.join(__dirname, "public")));
 
-        app.use("/", indexRouter);
-        app.use("/api", apiRouter);
+        // app.use("/", indexRouter);
+        // app.use("/api", apiRouter);
 
-        app.listen(process.env.PORT, () => {
+        app.get("/", (req, res) => {
+            res.sendFile(path.join(__dirname, "views/index.html"));
+        });
+
+        const bodyParser = require("body-parser");
+
+        const axios = require("axios");
+
+        const dbOp = require("./db/db");
+
+        app.get("/api/shorturl/:url", (req, res) => {
+            const url = Number(req.params["url"]);
+
+            dbOp.getShortURL(url)
+                .then((doc) => {
+                    if (!doc) {
+                        return res.json({
+                            error: `Shorturl ${url} does not exist`,
+                        });
+                    }
+
+                    res.redirect(doc["long-url"]);
+                })
+                .catch((err) => res.json({ error: "err" }));
+        });
+
+        // Parse the request body to make the url available
+        // on req.body["long-url"]
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(bodyParser.json());
+
+        app.post("/api/shorturl", (req, res) => {
+            const url = req.body["long-url"];
+
+            const urlErrors = {
+                ENOTFOUND: "Invalid hostname",
+            };
+
+            axios
+                .get(url)
+                .then((response) => {
+                    dbOp.addLongURL(url).then((doc) => {
+                        res.json({
+                            original_url: doc["long-url"],
+                            short_url: doc["short-url"],
+                        });
+                    });
+                })
+                .catch((err) => {
+                    res.json({ error: urlErrors[err.code] });
+                });
+        });
+
+        app.listen(port, () => {
             console.log(`Express is listening on port ${process.env.PORT}`);
         });
     })
