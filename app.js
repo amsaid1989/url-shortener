ad75d28549d091f542b684222844b15bf412fac0
Abdelrahman <said.abdelrahman89@gmail.com>
2021-05-07 14:16:31 +0100

    Express server only loads if the connection to the database is successful

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 require("dotenv").config();
 
 const express = require("express");
-const app = express();
-
 const path = require("path");
-
 const cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 }));
-
 const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 const apiRouter = require("./routes/api");
-
-app.use(express.static(path.join(__dirname, "public")));
 
 mongoose
     .connect(process.env.MONGO_URL, {
@@ -24,13 +17,19 @@ mongoose
     .then((conn) => {
         console.log("DB connected successfully");
 
+        const app = express();
+
+        app.use(cors({ optionsSuccessStatus: 200 }));
+
+        app.use(express.static(path.join(__dirname, "public")));
+
         app.use("/", indexRouter);
         app.use("/api", apiRouter);
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Express is listening on port ${process.env.PORT}`);
+        });
     })
     .catch((err) => {
         console.log(err);
     });
-
-app.listen(process.env.PORT, () => {
-    console.log(`Express is listening on port ${process.env.PORT}`);
-});
